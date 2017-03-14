2387ee710286442f9636c025791f4c217286160f
markpineda <markpineda7@gmail.com>
2017-03-14 05:14:21 -0400

    cleanup

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+// Used for production mode
+// ************************
+// import {enableProdMode} from '@angular/core';
+//
+// enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule);
